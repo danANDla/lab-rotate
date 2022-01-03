140a14e6ed7bce3f75fbdd6e5ffba8cc073b7efe
danANDla <d.a.n.202021@gmail.com>
2022-01-04 02:35:42 +0300

    deleted reduntant functions in bmp_reader

--- bmp_reader.c
+++ bmp_reader.c
@@ -20,47 +20,17 @@ void print_header(const struct bmp_header* const header) {
     printf("--------------------------/bmp_header/--------------------------\n");
 }
 
-static bool read_header(struct bmp_header* const header, FILE* in) {
-    return fread(header, sizeof(struct bmp_header), 1, in);
-}
-
-static bool read_string(void* const pos, size_t width, FILE* in) {
-    size_t len = fread(pos, sizeof(struct pixel), width, in);
-    if (len != width) {
-        if (feof(in)) printf("Premature end of file.\n");
-        printf("Bad read: %u\n", len);
-        printf("ftell %d\n", ftell(in));
-    }
-    return len == width;
-}
-
 enum read_status from_bmp(FILE* in, struct image* img) {
     struct bmp_header new_header;
-    if (!read_header(&new_header, in)) return READ_INVALID_HEADER;
-    print_header(&new_header);
+    if (!fread(&new_header, sizeof(struct bmp_header), 1, in)) return READ_INVALID_HEADER;
 
     *img = create_image(new_header.biHeight, new_header.biWidth);
-    printf("Image size: %" PRIu64 "x%" PRIu64 "\n", img->height, img->width);
     const uint8_t offset = calculate_padding(new_header.biWidth);
-    printf("Offset %" PRIu8 "\n", offset);
 
-    /*for (size_t i = 0; i < new_header.biHeight; ++i) {
-        void* pos = img->pixels + img->width * i;
-        if (!read_string(pos, new_header.biWidth, in)) {
-            printf("%u, %" PRId64 "\n", i, img->height);
-            return READ_INVALID_BITS;
-        }
-        fread(pos, sizeof(struct pixel), new_header.biWidth, in);
-        fseek(in, offset, SEEK_CUR);
-    }*/
     fseek(in, new_header.bOffBits, SEEK_SET);
     for (size_t i = 0; i < new_header.biHeight; i++) {
-        void* start = img->pixels + img->width * i;
-        if (!fread(start, sizeof(struct pixel) , new_header.biWidth, in)) {
-            printf("%u, %" PRId64 "\n", i, img->height);
-            return READ_INVALID_BITS;
-            fseek(in, offset, SEEK_CUR);
-        }        
+        void* pos = img->pixels + img->width * i;
+        if (!fread(pos, sizeof(struct pixel) , new_header.biWidth, in)) return READ_INVALID_BITS;        
         fseek(in, offset, SEEK_CUR);
     }
 
