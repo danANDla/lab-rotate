4e31f239008d2d0b38ad725fdb1c3b865b76505e
archdla <d.a.n.202021@gmail.com>
2022-01-05 15:35:44 +0300

    changed file names in includes and added args

--- Makefile
+++ Makefile
@@ -12,5 +12,17 @@ $(TARGET): $(OBJFILES)
 %.o: %.c
 	$(CC) -g -O -c $<
 
+image.o: image/image.c
+	$(CC) -g -O -c $<
+
+bmp_writer.o: bmp_util/bmp_writer.c
+	$(CC) -g -O -c $<
+
+bmp_reader.o: bmp_util/bmp_reader.c
+	$(CC) -g -O -c $<
+
+file_io.o: file_io/file_io.c
+	$(CC) -g -O -c $<
+
 clean:
 	rm -f $(OBJFILES) 
--- main.c
+++ main.c
@@ -1,49 +1,58 @@
 #include"lib.h"
 
-int main() {
-	char* fname1 = "./tests/image1.bmp";
-	char* fname2 = "./tests/image1res.bmp";
-	char* fname3 = "./tests/image1resres.bmp";
+int main(int args, char **argv) {
+//	char* fname1 = "./tests/image1.bmp";
+//	char* fname2 = "./tests/image1res.bmp";
+//	char* fname3 = "./tests/image1resres.bmp";
+    if (args < 3) {
+        printf("too few arguments\n");
+        return 0;
+    }
+    if (args > 3) {
+        printf("too many arguments\n");
+        return 0;
+    }
+    const char *fname1 = argv[1];
+    const char *fname2 = argv[2];
 
-	FILE* source;
-	if (open_r_file(fname1, &source) != READ_FILE_OK) {
-		printf("smth wrong\n");
-		return;
-	}
-	struct image new_image;
-	switch (from_bmp(source, &new_image))
-	{
-		case READ_INVALID_HEADER:
-			printf("invalid header\n");
-			break;
-		case READ_INVALID_BITS:
-			printf("invalid bits\n");
-			break;
-		case READ_OK:
-			printf("read succses\n");
-		default:
-			break;
-	}
-	close_file(source);
+    FILE *source;
+    if (open_r_file(fname1, &source) != READ_FILE_OK) {
+        printf("smth wrong with opening a file\n");
+        return 1;
+    }
+    struct image new_image;
+    switch (from_bmp(source, &new_image)) {
+        case READ_INVALID_HEADER:
+            printf("invalid header\n");
+            break;
+        case READ_INVALID_BITS:
+            printf("invalid bits\n");
+            break;
+        case READ_OK:
+            printf("read succses\n");
+        default:
+            break;
+    }
+    close_file(source);
 
-	FILE* result;
-	if (open_w_file(fname2, &result) != WRITE_FILE_OK) {
-		printf("smth wrong\n");
-		return;
-	}
+    FILE *result;
+    if (open_w_file(fname2, &result) != WRITE_FILE_OK) {
+        printf("smth wrong\n");
+        return 1;
+    }
 
-	switch (to_bmp(result, &new_image))
-	{
-		case WRITE_STRING_ERROR:
-			printf("write string error\n");
-		case WRITE_HEADER_ERROR:
-			printf("write header error\n");
-		case WRITE_PADDING_ERROR:
-			printf("write padding error\n");
-		case WRITE_ERROR:
-			printf("write error\n");
-		default:
-			break;
-	}
-	close_file(result);
+    switch (to_bmp(result, &new_image)) {
+        case WRITE_STRING_ERROR:
+            printf("write string error\n");
+        case WRITE_HEADER_ERROR:
+            printf("write header error\n");
+        case WRITE_PADDING_ERROR:
+            printf("write padding error\n");
+        case WRITE_ERROR:
+            printf("write error\n");
+        default:
+            break;
+    }
+    close_file(result);
+    return 0;
 }
